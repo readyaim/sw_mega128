f0ed3140e0c4a3527eb929b65f99afe7914dd841
readyaim <shangzhun1978@outlook.com>
2018-08-06 20:35:43 +0800

    20180806

---
+++ to_Udisk.bat
@@ -0,0 +1,22 @@
+@echo off
+::Mega128A
+Set C_FILES=*.c
+Set H_FILES=*.h
+Set BAT_FILES=*.bat
+Set SRC_FOLDER1=C:\Myfolder\Project_sz\AVR_prj\mega128\sw_mega128
+Set OBJ_FOLDER1=D:\mega128\sw_mega128
+
+
+echo Back to U_Disk
+
+@echo off
+pause
+
+::copy sw_mega128 to U_disk
+echo d|xcopy "%SRC_FOLDER1%\%C_FILES%" "%OBJ_FOLDER1%\%C_FILES%" /r/y/D
+echo d|xcopy "%SRC_FOLDER1%\%H_FILES%" "%OBJ_FOLDER1%\%H_FILES%" /r/y/D
+echo d|xcopy "%SRC_FOLDER1%\%BAT_FILES%" "%OBJ_FOLDER1%\%BAT_FILES%" /r/y/D
+
+
+
+pause
